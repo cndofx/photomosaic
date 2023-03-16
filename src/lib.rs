6e416bc6eee41d9ebd0ba2ce6967de51cb5e49ef
cndofx <19335833+cndofx@users.noreply.github.com>
2023-03-16 04:12:50 -0600

    add `average_image_color` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use image::DynamicImage;
+use image::{DynamicImage, GenericImageView};
 
 use cells::CELL_SIZE;
 
@@ -10,4 +10,24 @@ fn crop_image(image: &mut DynamicImage) -> DynamicImage {
     let cropped_width = image_width - (image_width % CELL_SIZE);
     let cropped_height = image_height - (image_height % CELL_SIZE);
     image.crop(0, 0, cropped_width, cropped_height)
+}
+
+fn average_image_color(image: &DynamicImage) -> (u8, u8, u8) {
+    let mut r: u64 = 0;
+    let mut g: u64 = 0;
+    let mut b: u64 = 0;
+    let mut count: u64 = 0;
+
+    for (_, _, color) in image.pixels() {
+        r += color.0[0] as u64;
+        g += color.0[1] as u64;
+        b += color.0[2] as u64;
+        count += 1;
+    }
+
+    r /= count;
+    g /= count;
+    b /= count;
+
+    (r as u8, g as u8, b as u8)
 }
