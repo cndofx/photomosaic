6f894463e8f84b4d21f9cd7701e02f9a841d9eac
cndofx <19335833+cndofx@users.noreply.github.com>
2023-03-16 01:14:45 -0600

    crop image to a multiple of the cell size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use image::{io::Reader as ImageReader, GenericImageView};
+
+const CELL_SIZE: u32 = 50;
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = ImageReader::open("img/input.png")
+        .unwrap()
+        .decode()
+        .unwrap();
+
+    let width = img.width();
+    let height = img.height();
+    dbg!(width, height);
+
+    let new_width = width - (width % CELL_SIZE);
+    let new_height = height - (height % CELL_SIZE);
+    dbg!(new_width, new_height);
+
+    let cells_x = new_width / CELL_SIZE;
+    let cells_y = new_height / CELL_SIZE;
+    dbg!(cells_x, cells_y);
+
+    let new_image = img.crop(0, 0, new_width, new_height);
+    new_image.save("img/output.png").unwrap();
+
+    // for (x, y, color) in new_image.pixels() {
+    //     println!("pixel belongs to cell at ({}, {})", x / CELL_SIZE, y / CELL_SIZE);
+    // }
+
+    // dbg!(&img);
 }
